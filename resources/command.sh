82b48ff5a7738f9e0eee630bc4040f23730bafe8
Joris De Winne <joris.dewinne@gmail.com>
2018-10-17 10:02:14 -0700

    Using APP_HOME

--- resources/command.sh
+++ resources/command.sh
@@ -2,19 +2,19 @@
 
 # Copy downloads into 'plugins'
 if [ -d "/data/build/downloads/plugins" ]; then
-    cp /data/build/downloads/plugins/* /opt/xl-release-server/default-plugins/__local__
+    cp /data/build/downloads/plugins/* ${APP_HOME}/default-plugins/__local__
 fi
 
 # Copy to plugins
-cp /data/build/libs/*.jar /opt/xl-release-server/default-plugins/__local__
+cp /data/build/libs/*.jar ${APP_HOME}/default-plugins/__local__
 
 # Link the license
-ln -s /license/xl-release-license.lic /opt/xl-release-server/default-conf/xl-release-license.lic
+ln -s /license/xl-release-license.lic ${APP_HOME}/default-conf/xl-release-license.lic
 
 # Link `ext` folder
-find /data/src/main/resources -maxdepth 1 -mindepth 1 -type d -exec ln -s -f '{}' /opt/xl-release-server/ext/ \;
+find /data/src/main/resources -maxdepth 1 -mindepth 1 -type d -exec ln -s -f '{}' ${APP_HOME}/ext/ \;
 
 # Start XLR and
 export ADMIN_PASSWORD=admin
 export REPOSITORY_KEYSTORE_PASSPHRASE=admin
-/usr/bin/dumb-init /opt/xl-release-server/bin/run-in-container.sh
+/opt/xebialabs/tini ${APP_HOME}/bin/run-in-container.sh
