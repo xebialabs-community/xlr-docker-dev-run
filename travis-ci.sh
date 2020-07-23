44dcccc139f59fadc4190ee9f0a79bcabce12287
Admin <rick_broker@yahoo.com>
2020-07-23 15:20:01 -0400

    Include dockertags script in repo

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
 {
@@ -8,7 +8,7 @@ TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
 }
 " https://hub.docker.com/v2/users/login/ | jq -r .token)
 
-http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/2ca20118482b7790fdbc0a1e3cbe6a3d4e78933b/dockertags > ./dockertags
+#http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/2ca20118482b7790fdbc0a1e3cbe6a3d4e78933b/dockertags > ./dockertags
 chmod +x dockertags
 
 image_name="xebialabsunsupported/xlr_dev_run"
@@ -20,7 +20,7 @@ while read tag ; do
     echo "####        $image_name:$tag builing"
     docker build -t $image_name:$tag --build-arg xlr_tag=$tag .
     echo "####        $image_name:$tag built"
-    docker push $image_name:$tag
+    #docker push $image_name:$tag
     echo "####        $image_name:$tag pushed"
     echo "####################################################################"
     docker push $image_name:$tag
